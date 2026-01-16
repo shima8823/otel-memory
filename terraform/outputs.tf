6646c6328440f62d57ac840da2e064419d6ae0c0
shima8823 <taiyaki1275@gmail.com>
2026-01-16 15:32:33 +0900

    Refactor: Remove unused output variables and add project_id output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,51 +1,11 @@
-output "collector_instance_name" {
-  description = "Name of the Collector VM instance"
-  value       = google_compute_instance.collector_vm.name
-}
-
-output "collector_zone" {
-  description = "Zone of the Collector VM instance"
-  value       = google_compute_instance.collector_vm.zone
-}
-
-output "collector_external_ip" {
-  description = "External IP address of the Collector VM"
-  value       = google_compute_instance.collector_vm.network_interface[0].access_config[0].nat_ip
+output "project_id" {
+  description = "Project ID"
+  value       = var.project_id
 }
 
 output "collector_internal_ip" {
   description = "Internal IP address of the Collector VM (used by Loadgen VM)"
   value       = google_compute_instance.collector_vm.network_interface[0].network_ip
-}
-
-output "collector_gcloud_ssh_command" {
-  description = "gcloud SSH command to connect to the Collector VM"
-  value       = "gcloud compute ssh ${google_compute_instance.collector_vm.name} --zone=${google_compute_instance.collector_vm.zone} --project=${var.project_id}"
-}
-
-output "loadgen_instance_name" {
-  description = "Name of the Loadgen VM instance"
-  value       = google_compute_instance.loadgen_vm.name
-}
-
-output "loadgen_zone" {
-  description = "Zone of the Loadgen VM instance"
-  value       = google_compute_instance.loadgen_vm.zone
-}
-
-output "loadgen_external_ip" {
-  description = "External IP address of the Loadgen VM"
-  value       = google_compute_instance.loadgen_vm.network_interface[0].access_config[0].nat_ip
-}
-
-output "loadgen_internal_ip" {
-  description = "Internal IP address of the Loadgen VM"
-  value       = google_compute_instance.loadgen_vm.network_interface[0].network_ip
-}
-
-output "loadgen_gcloud_ssh_command" {
-  description = "gcloud SSH command to connect to the Loadgen VM"
-  value       = "gcloud compute ssh ${google_compute_instance.loadgen_vm.name} --zone=${google_compute_instance.loadgen_vm.zone} --project=${var.project_id}"
 }
 
 output "grafana_url" {
@@ -66,9 +26,4 @@ output "jaeger_url" {
 output "collector_metrics_url" {
   description = "URL to access Collector self-telemetry metrics"
   value       = "http://${google_compute_instance.collector_vm.network_interface[0].access_config[0].nat_ip}:8888/metrics"
-}
-
-output "loadgen_command_example" {
-  description = "Example loadgen command with Collector internal IP"
-  value       = "./loadgen -endpoint ${google_compute_instance.collector_vm.network_interface[0].network_ip}:4317 -scenario sustained -duration 60s -rate 1000"
 }
