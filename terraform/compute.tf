6faa33d30dfe783e6d9140a82874aa36f72ef565
shima8823 <taiyaki1275@gmail.com>
2026-01-16 16:08:52 +0900

    Refactor style guideを参考にリソース定義の順番を変更

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,15 +1,16 @@
 // OTel Collector, Prometheus, Grafana, Jaeger を実行
 resource "google_compute_instance" "collector_vm" {
-  name         = "otel-collector"
-  machine_type = var.collector_machine_type
-  zone         = var.zone
-
-  tags = local.collector_tags
+  name                      = "otel-collector"
+  machine_type              = var.collector_machine_type
+  zone                      = var.zone
+  tags                      = local.collector_tags
+  labels                    = local.collector_labels
+  allow_stopping_for_update = true
 
   boot_disk {
     initialize_params {
       image = data.google_compute_image.ubuntu.self_link
-      size  = 30
+      size  = 20
       type  = "pd-standard"
     }
   }
@@ -24,19 +25,16 @@ resource "google_compute_instance" "collector_vm" {
   metadata_startup_script = templatefile("${path.module}/startup-script.sh", {
     git_repo_url = "https://github.com/shima8823/otel-memory.git"
   })
-
-  allow_stopping_for_update = true
-
-  labels = local.collector_labels
 }
 
 // 負荷生成ツール (loadgen) を実行
 resource "google_compute_instance" "loadgen_vm" {
-  name         = "otel-loadgen"
-  machine_type = var.loadgen_machine_type
-  zone         = var.zone
-
-  tags = local.loadgen_tags
+  name                      = "otel-loadgen"
+  machine_type              = var.loadgen_machine_type
+  zone                      = var.zone
+  tags                      = local.loadgen_tags
+  labels                    = local.loadgen_labels
+  allow_stopping_for_update = true
 
   boot_disk {
     initialize_params {
@@ -58,9 +56,5 @@ resource "google_compute_instance" "loadgen_vm" {
     collector_internal_ip = google_compute_instance.collector_vm.network_interface[0].network_ip
   })
 
-  allow_stopping_for_update = true
-
   depends_on = [google_compute_instance.collector_vm]
-
-  labels = local.loadgen_labels
 }
--- terraform/network.tf
+++ terraform/network.tf
@@ -1,63 +1,55 @@
 // SSH: 両VMへの外部アクセス
 resource "google_compute_firewall" "allow_ssh" {
-  name    = "otel-debug-allow-ssh"
-  network = "default"
+  name          = "otel-debug-allow-ssh"
+  description   = "Allow SSH access from specified IPs"
+  network       = "default"
+  source_ranges = var.allowed_ssh_ips
+  target_tags   = ["allow-ssh"]
 
   allow {
     protocol = "tcp"
     ports    = ["22"]
   }
-
-  source_ranges = var.allowed_ssh_ips
-  target_tags   = ["allow-ssh"]
-
-  description = "Allow SSH access from specified IPs"
 }
 
 // Web UI: Collector VMへの外部アクセス（Grafana, Prometheus, Jaeger）
 resource "google_compute_firewall" "allow_web_ui" {
-  name    = "otel-debug-allow-web-ui"
-  network = "default"
+  name          = "otel-debug-allow-web-ui"
+  description   = "Allow access to Grafana (3000), Prometheus (9090), Jaeger (16686)"
+  network       = "default"
+  source_ranges = var.allowed_web_ips
+  target_tags   = ["allow-web-ui"]
 
   allow {
     protocol = "tcp"
     ports    = ["3000", "9090", "16686"]
   }
-
-  source_ranges = var.allowed_web_ips
-  target_tags   = ["allow-web-ui"]
-
-  description = "Allow access to Grafana (3000), Prometheus (9090), Jaeger (16686)"
 }
 
 // OTLP gRPC: 内部ネットワークからのみ（Loadgen VM → Collector VM）
 resource "google_compute_firewall" "allow_otlp_internal" {
-  name    = "otel-debug-allow-otlp-internal"
-  network = "default"
+  name          = "otel-debug-allow-otlp-internal"
+  description   = "Allow OTLP gRPC traffic from internal network (Loadgen VM)"
+  network       = "default"
+  source_ranges = ["10.128.0.0/9"]
+  target_tags   = ["otel-collector"]
 
   allow {
     protocol = "tcp"
     ports    = ["4317"]
   }
-
-  source_ranges = ["10.128.0.0/9"]
-  target_tags   = ["otel-collector"]
-
-  description = "Allow OTLP gRPC traffic from internal network (Loadgen VM)"
 }
 
 // Collector Metrics: 外部からのセルフテレメトリアクセス（オプション）
 resource "google_compute_firewall" "allow_collector_metrics" {
-  name    = "otel-debug-allow-collector-metrics"
-  network = "default"
+  name          = "otel-debug-allow-collector-metrics"
+  description   = "Allow access to Collector self-telemetry metrics"
+  network       = "default"
+  source_ranges = var.allowed_web_ips
+  target_tags   = ["otel-collector"]
 
   allow {
     protocol = "tcp"
     ports    = ["8888"]
   }
-
-  source_ranges = var.allowed_web_ips
-  target_tags   = ["otel-collector"]
-
-  description = "Allow access to Collector self-telemetry metrics"
 }
